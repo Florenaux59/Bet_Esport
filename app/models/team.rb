2794ea673aebaafcca0300697ca479361887cc4f
Simon Chrétien <simon.chretien62@gmail.com>
2024-06-06 15:50:08 +0200

    Seed all teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,5 @@
 class Team < ApplicationRecord
   has_many :matches_as_1, class_name: "Match", foreign_key: :team1_id
   has_many :matches_as_2, class_name: "Match", foreign_key: :team2_id
-
-  has_many :games, through: :matches_as_1
+  belongs_to :game
 end
