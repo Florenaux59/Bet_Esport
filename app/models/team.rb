a05ede0c0d4f0fb9924ea5dbde0328e1764f6ef5
collet remi <remi.collet.perso@hotmail.com>
2024-06-06 17:39:33 +0200

    create teams and matchs with api in seeds

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
-  has_many :matches_as_1, class_name: "Match", foreign_key: :team1_id
-  has_many :matches_as_2, class_name: "Match", foreign_key: :team2_id
+  has_many :matches_as_1, class_name: "Match", foreign_key: :team1_id, dependent: :destroy
+  has_many :matches_as_2, class_name: "Match", foreign_key: :team2_id, dependent: :destroy
   belongs_to :game
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,12 @@ require 'net/http'
 require 'json'
 
 Team.destroy_all
-
-
+Game.destroy_all
 valorant = Game.create!(name: "Valorant", slug: "valorant")
 rocket = Game.create!(name: "Rocket League", slug: "rl")
 lol = Game.create!(name: "League of Legends", slug: "league-of-legends")
-puts "---------------------------début création team valo--------------------------"
 
+puts "---------------------------début création team valo--------------------------"
 counter = 1
 14.times do
   url = URI("https://api.pandascore.co/valorant/teams?page=#{counter}&per_page=100")
@@ -29,19 +28,16 @@ counter = 1
   array = JSON.parse(response_body)
   p array.size
 
-
   array.each do |team_valo|
     Team.create(team_name: team_valo["name"], game: valorant)
-
   end
-counter += 1
-
+  counter += 1
 end
 puts "---------------------------fin création team valo--------------------------"
-puts "---------------------------début création team rl--------------------------"
 
+puts "---------------------------début création team rl--------------------------"
 counter = 1
-  4.times do
+4.times do
   url2 = URI("https://api.pandascore.co/rl/teams?page=#{counter}&per_page=100")
   http2 = Net::HTTP.new(url2.host, url2.port)
   http2.use_ssl = true
@@ -63,7 +59,6 @@ end
 puts "---------------------------fin création team rl--------------------------"
 
 puts "---------------------------début création team lol--------------------------"
-
 counter = 1
 16.times do
   url3 = URI("https://api.pandascore.co/lol/teams?page=#{counter}&per_page=100")
@@ -88,21 +83,25 @@ puts "---------------------------fin création team lol-------------------------
 
 puts "---------------------------création match --------------------------"
 
-# url = URI("https://api.pandascore.co/lol/matches/upcoming")
+url4 = URI("https://api.pandascore.co/lol/matches/upcoming")
 
-# http = Net::HTTP.new(url.host, url.port)
-# http.use_ssl = true
+http4 = Net::HTTP.new(url4.host, url4.port)
+http4.use_ssl = true
 
-# request = Net::HTTP::Get.new(url)
-# request["accept"] = 'application/json'
+request4 = Net::HTTP::Get.new(url4)
+request4["accept"] = 'application/json'
 
-# request["authorization"] = 'Bearer OFZGCDoGWBUv4oLqD5LJcynh4p0Ad-L-9Ln1HkGA968MmOggmYg'
+request4["authorization"] = 'Bearer OFZGCDoGWBUv4oLqD5LJcynh4p0Ad-L-9Ln1HkGA968MmOggmYg'
 
-# response = http.request(request)
-# matches = JSON.parse(response.read_body)
-
-
-# start = matches.first["begin_at"]
+response4 = http4.request(request4)
+matches = JSON.parse(response4.read_body)
 
 
-# p matches.first
+matches.each do |match|
+  team1 = Team.find_by(team_name: match["opponents"][0]["opponent"]["name"])
+  p "VERSUS"
+  team2 = Team.find_by(team_name: match["opponents"][1]["opponent"]["name"])
+  Match.create!(team1: team1, team2: team2, start_date: match["begin_at"], game: lol)
+  puts "--------------------------------------------------"
+end
+puts "---------------------------fin création match --------------------------"
