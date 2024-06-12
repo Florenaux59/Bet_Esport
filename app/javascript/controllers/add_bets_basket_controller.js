845e5793c40d1864956e9cfbbab96078009b9a41
Nicolas <nicolas.lisardipro@gmail.com>
2024-06-12 12:25:29 +0200

    panier

--- app/javascript/controllers/add_bets_basket_controller.js
+++ app/javascript/controllers/add_bets_basket_controller.js
@@ -2,12 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="addbetspanier"
 export default class extends Controller {
-  static targets = ["team1", "team2"]
+  static targets = ["team1", "team2", "cards", "display"]
   connect() {
     console.log("hello");
   }
  basket(){
-
-  this.team1Target.classList.add("")
+  console.log(this.displayTarget)
+  this.cardsTarget.style.gridTemplateColumns = "1fr 1fr"
+  this.displayTarget.classList.remove("d-none")
  }
 }
