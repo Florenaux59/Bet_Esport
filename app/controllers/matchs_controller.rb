b2947931eeaa1c5af8e03a9095cdad32a7deb1dc
collet remi <remi.collet.perso@hotmail.com>
2024-06-13 17:55:50 +0200

    dza

--- app/controllers/matchs_controller.rb
+++ app/controllers/matchs_controller.rb
@@ -5,11 +5,6 @@ class MatchsController < ApplicationController
   def index
     @matchs = Match.includes(:team1, :team2, :game).all
     @games = Game.all
-<<<<<<< bet_13062024
-    #FakeJob.perform_now
-=======
-    # FakeJob.perform_now
->>>>>>> master
   end
 
   def show
